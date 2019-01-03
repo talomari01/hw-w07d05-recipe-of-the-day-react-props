9d030c3033dff281bfb27c3d3a55db7342888dd0
trevorpreston <trevorpreston4@gmail.com>
2019-01-03 16:23:20 +0300

    remove submodule

---
+++ recipe-of-the-day/src/data/recipeOfTheDay.js
@@ -0,0 +1,28 @@
+const recipeOfTheDay = {
+  name: "Grandma's Mashed Potatoes",
+  description: "This recipe makes 4 mashed potatoes, just like my grandmother from the greate state of North Dakota used to make me.  The key here is to cook the potatoes properly, mash them, then present them on a plate.",
+  ingredients: [
+    {
+      ingredient: 'Potatoes',
+      amount: 5
+    },
+    {
+      ingredient: 'Water',
+      amount: '1 L'
+    },
+    {
+      ingredient: 'Salt',
+      amount: '2 Teaspoons'
+    },
+    {
+      ingredient: 'Butter',
+      amount: '2 Tablespoons'
+    },
+    {
+      ingredient: 'Buttersalt',
+      amount: '4 pinches'
+    },
+  ]
+}
+
+export default recipeOfTheDay;
---
+++ recipe-of-the-day/src/data/recipes.js
@@ -0,0 +1,24 @@
+const recipes = [
+  {
+    name: 'Chunky Baked Bean Sandwich'
+    servings: 8,
+    category: 'appetizer'
+  },
+  {
+    name: 'Balogna Sandwich',
+    servings: 1,
+    category: 'lunch'
+  },
+  {
+    name: 'Doritos',
+    servings: 80,
+    category: 'snack'
+  },
+  {
+    name: 'Gatorade Infused Dates',
+    servings: 12,
+    category: 'appetizer'
+  }
+]
+
+export default recipes;
